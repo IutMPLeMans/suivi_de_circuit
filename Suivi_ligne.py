5a543df0bf8727c9972dbdb34e1df120956e464c
s176676 <s176676@MPS0718-07>
2020-03-09 11:27:11 +0100

    modif fonction difficulté et affichage zone d'étude

--- Suivi_ligne.py
+++ Suivi_ligne.py
@@ -13,36 +13,40 @@ if not video.isOpened():
     exit()
 print('OK')
 
+
 #définition de la taille du rectangle à analyser
 def fdifficulté(img):
     z=int(input("choisir niveau de difficulté 1,2,3"))
     if z==1:
-        x=img.shape[0]
-        y=img.shape[1]
+        h=img.shape[0]
+        l=img.shape[1]
     elif z==2:
-        x=img.shape[0]/2+img.shape[0]/4
-        y=img.shape[1]/2+img.shape[0]/4
+        h=img.shape[0]//2
+        l=img.shape[1]//2
     else:
-        x=(img.shape[0]/2+img.shape[0]/4)/2+(img.shape[0]/2+img.shape[0]/4)/4
-        y=(img.shape[1]/2+img.shape[0]/4)/2+(img.shape[1]/2+img.shape[0]/4)/4
-    return x, y 
+        h=img.shape[0]//4
+        l=img.shape[1]//4
+    return h, l 
 
 #calcul moyenne des pixels pour savoir si proche du noir 
 #(permet de gérer le niveau de difficulté) ainsi que le pourcentage de noir
-def fpourcentage(img):
-    ind = np.where(img<10)
+def fpourcentage(img,h,l):
+    centre=(img.shape[0]//2,img.shape[1]//2)
+    u=img[centre[0]-h//2:centre[0]+h//2,centre[1]-l//2:centre[1]+l//2]
+    ind = np.where(u<10)
     print(ind[0].shape)
-    fdifficulté(img)
-    p=x*y
+    p=h*l
     pourcentagenoir=(ind[0].shape[0]/p)*100
     moyenne_u = np.mean(u[ind])
     #print("moyenne des pixels", moyenne_u)
     #print("pourcentage noire dans l'image", pourcentagenoir)
-    return moyenne_u
+    return moyenne_u, cv.imshow("zone",u)
 
 #affichage video couleur et noir/blanc
 i=0
-while i<20: 
+ret, img = video.read()
+h,l=fdifficulté(img)
+while i<4000: 
     ret, img = video.read()
     i=i+1
     if ret:
@@ -51,9 +55,10 @@ while i<20:
         u=cv.cvtColor(img, cv.COLOR_BGR2GRAY); #on convertit en noir et blanc
         cv.imshow('videoNB',u)
         cv.waitKey(30)
-        print("ok", fpourcentage(u))  
+        fpourcentage(u,h,l)
+        print("ok")  
        
     else:
         print('Perdu')
         break
-cv.destroyAllWindows()	
+cv.destroyAllWindows()
